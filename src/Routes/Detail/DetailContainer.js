749278ee5de1b81c2157a7658245c6851868c2d9
dubbsong <dubbsong@gmail.com>
2019-08-13 13:23:53 +0900

    Set State

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -2,7 +2,19 @@ import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
 export default class extends React.Component {
+  state = {
+    loading: true,
+    result: null,
+    error: null
+  };
+
   render() {
-    return <DetailPresenter />;
+    return (
+      <DetailPresenter
+        loading={this.state.loading}
+        result={this.state.result}
+        error={this.state.error}
+      />
+    );
   }
 }
