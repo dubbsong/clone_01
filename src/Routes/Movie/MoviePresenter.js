90759e301220c78ea64a6a02a396f78004244b7c
dubbsong <dubbsong@gmail.com>
2019-08-13 14:47:25 +0900

    Set PropTypes

--- src/Routes/Movie/MoviePresenter.js
+++ src/Routes/Movie/MoviePresenter.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import MovieHeader from 'Components/MovieHeader';
+import PropTypes from 'prop-types';
 
-const MoviePresenter = () => <MovieHeader />;
+const MoviePresenter = ({
+  loading,
+  trending,
+  nowPlaying,
+  topRated,
+  upcoming,
+  error
+}) => <MovieHeader />;
+
+MoviePresenter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  trending: PropTypes.array,
+  nowPlaying: PropTypes.array,
+  topRated: PropTypes.array,
+  upcoming: PropTypes.array,
+  error: PropTypes.string
+};
 
 export default MoviePresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import TVHeader from 'Components/TVHeader';
+import PropTypes from 'prop-types';
 
-const TVPresenter = () => <TVHeader />;
+const TVPresenter = ({
+  loading,
+  trending,
+  onTheAir,
+  popular,
+  topRated,
+  error
+}) => <TVHeader />;
+
+TVPresenter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  trending: PropTypes.array,
+  onTheAir: PropTypes.array,
+  popular: PropTypes.array,
+  topRated: PropTypes.array,
+  error: PropTypes.string
+};
 
 export default TVPresenter;
